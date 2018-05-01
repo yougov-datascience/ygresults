1448c79c90ff1496fd1200df7e6cbcefa8b71c64
nick.pelikan <nick.pelikan@yougov.com>
2018-05-01 10:27:58 -0700

    detypo docs

--- R/upsert.R
+++ R/upsert.R
@@ -8,7 +8,7 @@
 #' @importFrom stringi stri_rand_strings
 #' @importFrom dplyr copy_to
 #' @importFrom glue glue
-#' @importFrom dbi dbSendQuery
+#' @importFrom DBI dbSendQuery
 #' @export
 upsert <- function(df, db, target_table){
     ## TODO: PUT SOME TYPE SAFETY HERE
