61cc6ae6f6fbb09e61e0412f3e749b06b9825e6a
nick.pelikan <nick.pelikan@yougov.com>
2018-08-15 16:56:04 -0700

    upload + integration test!!

---
+++ R/format_pct.R
@@ -0,0 +1,54 @@
+## formats precincts into list format for JSON munge+upload
+#' @importFrom purrr map2
+#' @importFrom stats na.omit
+format_pct <- function(pct_df){
+
+    osplit <- split(pct_df, pct_df$office)
+    olist <- purrr::map(osplit, function(off_df){
+
+        csplit <- split(off_df, off_df$candidate)
+        clist <- purrr::map(csplit, function(cand_df){
+            pty <- unique(cand_df$party)
+            if (length(pty) > 1){
+                stop("multiple parties submitted for the same candidate", call. = F)
+            }
+
+            vsplit <- split(cand_df, cand_df$votetype)
+            vlist <- purrr::map(vsplit, function(vt_df){
+                if (nrow(vt_df) > 1){
+                    stop("identical candidate or votetype submitted in precinct. please correct",
+                         call. = F)
+                }
+
+                vt_df$votes
+            })
+
+            list("party" = pty,
+                 "votes" = vlist)
+        })
+
+    })
+
+    out_list <- list()
+
+    for (i in c("state", "county", "district", "precinct")){
+        if (i == "district"){
+            on <- "cd"
+            ov <- unique(na.omit(pct_df[[i]]))
+        } else if (i == "precinct") {
+            on <- "name"
+            ov <- unique(pct_df[[i]])
+        } else {
+            on <- i
+            ov <- unique(pct_df[[i]])
+        }
+
+        if (length(ov) > 1) stop(paste0("non-unique values proviced to ", i), call. = F)
+
+        out_list[[on]] <- ov
+    }
+
+    out_list[['office']] <- olist
+
+    out_list
+}
--- R/results_upload.R
+++ R/results_upload.R
@@ -1,3 +1,7 @@
+
+#' @importFrom httr PUT
+#' @importFrom purrr map
+#' @importFrom glue glue
 results_upload <- function(df, election_code){
     api_key <- getOption("results_api_key", NA)
     if (is.na(api_key)){
@@ -9,5 +13,14 @@ results_upload <- function(df, election_code){
         stop("API url not found. Please save it in options as `results_api_url`", call. = F)
     }
 
-    put_base <- paste0(api_base_url, "{elec_code}/write")
+    put_base <- paste0(api_base_url, "{election_code}/write")
+
+    pct_ls <- split(df, paste0(df[['state']], df[['county']], df[['precinct']]))
+
+    payload <- list(
+        "precincts" = unname(purrr::map(pct_ls, format_pct))
+    )
+
+    httr::PUT(glue::glue(put_base), httr::add_headers(`x-api-key` = api_key),
+              body = payload, encode = "json")
 }
