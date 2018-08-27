43e9f1185cd72317a3dc5c079f8e10b11892ad5f
nick.pelikan <nick.pelikan@yougov.com>
2018-08-27 10:56:15 -0700

    edit to unpacking namespace for passing tests

--- tests/testthat/test-results_get.R
+++ tests/testthat/test-results_get.R
@@ -2,7 +2,7 @@ context("test-results_get.R")
 
 test_that("api retrieves and munges correct data", {
   httptest::with_mock_API({
-      df <- results_get("test_20180824", state="04")
+      df <- results_get("test_20180824", county="04013")
       expect_equal(nrow(df), 429)
       expect_equal(sum(df$votes), 156065)
   })
