65aacbc0eb417d5901b38532ba482827b8cfd2c6
Nick Pelikan <nick.pelikan@yougov.com>
2018-10-24 19:13:37 -0600

    raw tables for value checking

---
+++ data-raw/buildtables.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(jsonlite)
+
+
+precinct_values <- read_json("../precinct_data_api/precinct_data_api/precinct_ingest/values/pct_dict.json") %>%
+    map(function(c){
+        map(c, function(cd){
+            purrr::flatten_chr(cd)
+        })
+    })
+
+candidate_values <- read_json("../precinct_data_api/precinct_data_api/precinct_ingest/values/cand_dict.json") %>%
+    map(function(c){
+        map(c, function(of){
+            if (!is.null(names(of))){
+                map(of, function(cd){
+                    purrr::flatten_chr(cd)
+                })
+            } else {
+                purrr::flatten_chr(of)
+            }
+        })
+    })
+
+devtools::use_data(precinct_values, candidate_values, internal=TRUE)
